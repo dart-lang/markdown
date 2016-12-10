3350c47cd8e8b9a5a90afc20921a408e3b045cf7
Kevin Moore <kevmoo@google.com>
2016-12-09 19:45:03 -0800

    Update CommonMark bits to reference 0.27

--- tool/common_mark_stats.dart
+++ tool/common_mark_stats.dart
@@ -120,7 +120,7 @@ String indent(String s) => s.splitMapJoin('\n', onNonMatch: (n) => '    $n');
 
 void printVerboseFailure(
     String message, CommonMarkTestCase test, String expected, String actual) {
-  print('$message: http://spec.commonmark.org/0.26/#example-${test.example}');
+  print('$message: http://spec.commonmark.org/0.27/#example-${test.example}');
   print('input:');
   print(indent(test.markdown));
   print('expected:');
