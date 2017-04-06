27981f681473702cbf036db361692d107dae84a1
Kevin Moore <kevmoo@google.com>
2017-04-05 17:02:42 -0700

    move version test to a proper test

--- tool/travis.sh
+++ tool/travis.sh
@@ -20,15 +20,3 @@ if [[ -n "$dirty_code" ]]; then
 else
   echo All Dart source files are formatted.
 fi
-
-# Assert that versions match.
-pubspec_version=$(awk '/^version:/' pubspec.yaml |cut -f2 -d' ')
-bin_version=$(dart bin/markdown.dart --version )
-if [[ $pubspec_version != $bin_version ]]; then
-  echo Version mismatch:
-  echo "    From pubspec.yaml:         $pubspec_version"
-  echo "    From lib/src/version.dart: $bin_version"
-  exit 1
-else
-  echo Package versions match.
-fi
