2f13f87b9e6474205d150bf8c63daf7f8f972a41
Sam Rawlins <sam.rawlins@gmail.com>
2017-12-30 08:55:15 -0800

    Add big-list-of-naughty-strings tests

---
+++ tool/update_blns.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+import 'dart:io';
+
+String _blnsJsonRawUrl =
+    'https://github.com/minimaxir/big-list-of-naughty-strings/raw/master/blns.json';
+String _blnsFilePath = 'test/blns.dart';
+
+void main() async {
+  var client = new HttpClient();
+  var request = await client.getUrl(Uri.parse(_blnsJsonRawUrl));
+  var response = await request.close();
+  var json = JSON.decode(await response.transform(UTF8.decoder).join(''))
+      as List<String>;
+  var blnsContent = new StringBuffer('''
+// GENERATED FILE. DO NOT EDIT.
+//
+// This file was generated from big-list-of-naughty-strings's JSON file:
+// $_blnsJsonRawUrl
+// at ${new DateTime.now()} by the script, tool/update_blns.dart.
+
+''');
+  blnsContent.writeln('var blns = <String>[');
+  var ignored = <String>[];
+  for (var str in json) {
+    var escaped = str
+        .replaceAll(r'\', r'\\')
+        .replaceAll("'", r"\'")
+        .replaceAll(r'$', r'\$');
+    blnsContent.writeln("  '$escaped',");
+  }
+  blnsContent.writeln('];');
+  new File(_blnsFilePath)..writeAsStringSync(blnsContent.toString());
+  exit(0);
+}
