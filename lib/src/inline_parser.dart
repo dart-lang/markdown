5c94de2060b237f98891afce8eba34f7803a94b7
Louis Orleans <louis@orleans.io>
2018-01-24 00:14:22 -0800

    🎨 dartfmt

--- lib/src/inline_parser.dart
+++ lib/src/inline_parser.dart
@@ -290,7 +290,8 @@ class AutolinkExtensionSyntax extends InlineSyntax {
 
   @override
   bool tryMatch(InlineParser parser) {
-    var startMatch = pattern.matchAsPrefix(parser.source, (parser.pos > 0 ? parser.pos - 1 : 0));
+    var startMatch = pattern.matchAsPrefix(
+        parser.source, (parser.pos > 0 ? parser.pos - 1 : 0));
     if (startMatch != null) {
       // Write any existing plain text up to this point.
       parser.writeText();
