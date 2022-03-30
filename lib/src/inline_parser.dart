0bac4f689c2a93db611738b1018b9b1f72a5d9f5
Zhiguang Chen <zhiguang.c@gmail.com>
2022-03-30 20:15:13 +0200

    Do not allow reference link labels contain left brackets (#402)

--- lib/src/inline_parser.dart
+++ lib/src/inline_parser.dart
@@ -1118,6 +1118,8 @@ class LinkSyntax extends TagSyntax {
           buffer.writeCharCode(char);
         }
         buffer.writeCharCode(next);
+      } else if (char == $lbracket) {
+        return null;
       } else if (char == $rbracket) {
         break;
       } else {
