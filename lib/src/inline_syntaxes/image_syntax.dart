7987e1e5ea945e7620d7cd72dcf1f6655968e5c0
Jonas Termansen <sortie@google.com>
2022-04-26 20:13:55 +0200

    Remove dependency on third party package:charcode.
    The charcode package used to be first party but has become a third party
    package. This change inlines the needed constants from package:charcode
    in the style done by other packages that used to depend on it.
    
    This change is being done to simplify the security review of upgrading
    the Dart SDK DEPS as package:charcode is the only third party package in
    there and it's barely used in the SDK. package:markdown is the last
    package in the SDK DEPS to depend on package:charcode.
    
    Bug: b/230318198

--- lib/src/inline_syntaxes/image_syntax.dart
+++ lib/src/inline_syntaxes/image_syntax.dart
@@ -2,9 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:charcode/charcode.dart';
-
 import '../ast.dart';
+import '../charcode.dart';
 import '../util.dart';
 import 'link_syntax.dart';
 
