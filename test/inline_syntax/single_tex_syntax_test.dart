ceb58cb9abd98929048665a6efa664162a41575a
edan <ydu3328@naver.com>
2022-07-06 15:30:18 +0900

    test: :white_check_mark: SC-2290 텍스 구문 분석 테스트 코드 추가
    - 싱글 달러 인라인 구문 분석 테스트 추가
    - 더블 달러 인라인 구문 분석 테스트 추가
    - 더블 달러 블록 구문 분석 테스트 추가

---
+++ test/inline_syntax/single_tex_syntax_test.dart
@@ -0,0 +1,59 @@
+import 'package:markdown/markdown.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('싱글 달러 인라인 구문 파싱 테스트', () {
+    /// MD 다큐먼트 생성
+    final document = Document(
+      inlineSyntaxes: [
+        /// $ 구문 분석기 추가
+        SingleTexInlineSyntex(),
+      ],
+      encodeHtml: false,
+    );
+
+    /// 파싱결과
+    final result = document.parseInline(r'$f(x)$라서$f(a)$');
+
+    expect(result, hasLength(3));
+
+    expect(
+      result[0],
+      TypeMatcher<Element>()
+          .having(
+            (e) => e.textContent,
+            r'$ 구문 분석기에 걸러진 문자',
+            equals('f(x)'),
+          )
+          .having(
+            (e) => e.tag,
+            '태그',
+            'tex',
+          ),
+    );
+
+    expect(
+      result[1],
+      TypeMatcher<Text>().having(
+        (e) => e.textContent,
+        r'$ 구문 분석기에 걸러지지 않은 문자',
+        equals('라서'),
+      ),
+    );
+
+    expect(
+      result[2],
+      TypeMatcher<Element>()
+          .having(
+            (e) => e.textContent,
+            r'$ 구문 분석기에 걸러진 문자',
+            equals('f(a)'),
+          )
+          .having(
+            (e) => e.tag,
+            '태그',
+            'tex',
+          ),
+    );
+  });
+}
