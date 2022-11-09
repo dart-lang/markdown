fe889bc672649a3ad1e53c0dc4a5ec7db8b36212
Zhiguang Chen <zhiguang.c@gmail.com>
2022-11-09 05:56:43 +0100

    Some performance improvement on delimiter syntaxes (#485)

--- lib/src/inline_syntaxes/strikethrough_syntax.dart
+++ lib/src/inline_syntaxes/strikethrough_syntax.dart
@@ -2,6 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import '../charcode.dart';
 import 'delimiter_syntax.dart';
 
 /// Matches strikethrough syntax according to the GFM spec.
@@ -11,6 +12,7 @@ class StrikethroughSyntax extends DelimiterSyntax {
           '~+',
           requiresDelimiterRun: true,
           allowIntraWord: true,
+          startCharacter: $tilde,
           tags: [DelimiterTag('del', 2)],
         );
 }
