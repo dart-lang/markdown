22862cdeddf6729905aa88af708ab1001b704e9c
Zhiguang Chen <zhiguang.c@gmail.com>
2022-11-09 04:46:39 +0100

    Some performance improvement on delimiter syntaxes

--- lib/src/inline_syntaxes/emphasis_syntax.dart
+++ lib/src/inline_syntaxes/emphasis_syntax.dart
@@ -2,12 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import '../charcode.dart';
 import 'delimiter_syntax.dart';
 
 class EmphasisSyntax extends DelimiterSyntax {
   /// Parses `__strong__` and `_emphasis_`.
   EmphasisSyntax.underscore()
-      : super('_+', requiresDelimiterRun: true, tags: _tags);
+      : super(
+          '_+',
+          requiresDelimiterRun: true,
+          tags: _tags,
+          startCharacter: $underscore,
+        );
 
   /// Parses `**strong**` and `*emphasis*`.
   EmphasisSyntax.asterisk()
@@ -16,6 +22,7 @@ class EmphasisSyntax extends DelimiterSyntax {
           requiresDelimiterRun: true,
           allowIntraWord: true,
           tags: _tags,
+          startCharacter: $asterisk,
         );
 
   static final _tags = [DelimiterTag('em', 1), DelimiterTag('strong', 2)];
