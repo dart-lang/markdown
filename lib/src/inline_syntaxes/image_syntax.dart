198d8df961f83486f0a0fae20e097d8bba26d876
Zhiguang Chen <zhiguang.c@gmail.com>
2022-11-15 20:10:49 +0100

    Fix an ImageSyntax issue

--- lib/src/inline_syntaxes/image_syntax.dart
+++ lib/src/inline_syntaxes/image_syntax.dart
@@ -25,7 +25,15 @@ class ImageSyntax extends LinkSyntax {
     final element = Element.empty('img');
     final children = getChildren();
     element.attributes['src'] = destination;
-    element.attributes['alt'] = children.map((node) => node.textContent).join();
+    element.attributes['alt'] = children.map((node) {
+      // See https://spec.commonmark.org/0.30/#image-description.
+      // An image description may contain links. Fetch text from the alt
+      // attribute if this nested link is an image.
+      if (node is Element && node.tag == 'img') {
+        return node.attributes['alt'];
+      }
+      return node.textContent;
+    }).join();
     if (title != null && title.isNotEmpty) {
       element.attributes['title'] = normalizeLinkTitle(title);
     }
